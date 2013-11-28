3850a50208116e407c8459a7240387f2384816c3
Philip Collins <Philip.Collins@unh.edu>
2013-11-28 05:28:33 -0500

    more

--- static/ckeditor/config.js
+++ static/ckeditor/config.js
@@ -8,4 +8,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
     config.extraPlugins = 'onchange,divarea';
+
+    config.startupFocus = true;
 };
